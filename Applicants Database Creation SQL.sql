91dd5f76ae2a26e39dd9f7d6f5ce6208eafb701c
nevine-MG <nevinemgouda@gmail.com>
2015-05-26 14:50:51 +0200

    Applicants Database Creation SQL file corrected

--- Applicants Database Creation SQL.sql
+++ Applicants Database Creation SQL.sql
@@ -442,8 +442,6 @@ ALTER TABLE `relationship_financial_info`
 --
 ALTER TABLE `admission_info`
 ADD CONSTRAINT `admission_info_FK` FOREIGN KEY (`ApplicationID`) REFERENCES `applicant` (`ApplicationID`) ON DELETE CASCADE ON UPDATE CASCADE,
-ADD CONSTRAINT `admission_info_ibfk_1` FOREIGN KEY (`IntendedMajor`) REFERENCES `enrollements`.`major` (`MajorID`) ON DELETE CASCADE ON UPDATE CASCADE,
-ADD CONSTRAINT `applied_school_fk` FOREIGN KEY (`AppliedSchool`) REFERENCES `enrollements`.`school` (`SchoolID`) ON DELETE CASCADE ON UPDATE CASCADE,
 ADD CONSTRAINT `applied_semester_FK` FOREIGN KEY (`SemesterOfApplication`) REFERENCES `applicant_applied_semester` (`AppliedSemester`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
